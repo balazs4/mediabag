c28de5e4015e895165d915801471451fce50f83b
balazs4 <balazs4web@gmail.com>
2017-10-08 15:29:33 +0200

    feat: add local media support
    `FOLDER` env variable should define the fullpath
    to the media folder, which will be read recursively
    name will be the basename of the path,
    and tags is the dirname of the path
    The result of the DB and FOLDER will be merged

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const request = require('request-promise-native');
+const readdir = require('recursive-readdir');
 
-module.exports = async () => {
-  const flat = await request(process.env.DB, { json: true });
-  const array = Object.keys(flat).map(x =>
-    Object.assign({}, flat[x], { id: x })
+const { basename, dirname } = require('path');
+
+const streams = DB =>
+  // request the DB via http
+  request(DB, { json: true }).then(flat =>
+    // create array from flat-db object and the key as id
+    Object.keys(flat).map(x => Object.assign({}, flat[x], { id: x }))
   );
-  return array;
+
+const files = FOLDER =>
+  readdir(FOLDER).then(x =>
+    x.map(fullpath => ({
+      url: fullpath,
+      name: basename(fullpath),
+      id: basename(fullpath),
+      icon:
+        'http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/file-video-icon.png',
+      tags: [basename(dirname(fullpath))]
+    }))
+  );
+module.exports = async () => {
+  const results = await Promise.all([
+    streams(process.env.DB),
+    files(process.env.FOLDER)
+  ]);
+  return results.reduce((x, y) => x.concat(y), []);
 };
